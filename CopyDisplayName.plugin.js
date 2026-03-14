a02a6b7d248df65e2b7ad1d5b02cb6ef523375d0
ifeelgood0992-cmyk <ifeelgood0992@gmail.com>
2026-03-14 23:36:14 +0300

    Update CopyDisplayName.plugin.js

--- CopyDisplayName.plugin.js
+++ CopyDisplayName.plugin.js
@@ -1,53 +1,18 @@
 /**
  * @name CopyDisplayName
- * @author ChatGPT
+ * @author Paewast
  * @version 1.0.0
- * @description Adds a button to copy a user's display name.
+ * @description Adds a context menu button to copy a user's display name
+ * @source https://github.com/ifeelgood0992-cmyk/discord-plugin
  */
 
-module.exports = class CopyDisplayName {
-    start() {
-        this.patch();
-    }
-
-    stop() {
-        if (this.unpatch) this.unpatch();
-    }
-
-    patch() {
-        const { Patcher, WebpackModules, DiscordModules } = BdApi;
-
-        const UserContextMenu = WebpackModules.find(m =>
-            m?.default?.displayName === "UserContextMenu"
-        );
-
-        this.unpatch = Patcher.after(
-            "CopyDisplayName",
-            UserContextMenu,
-            "default",
-            (_, [props], ret) => {
-                const { React } = DiscordModules;
-
-                const MenuItem = WebpackModules.find(
-                    m => m?.default?.displayName === "MenuItem"
-                )?.default;
-
-                if (!MenuItem) return;
-
-                ret.props.children.push(
-                    React.createElement(MenuItem, {
-                        id: "copy-display-name",
-                        label: "Copy Display Name",
-                        action: () => {
-                            const name =
-                                props.user.globalName ||
-                                props.user.username;
-
-                            navigator.clipboard.writeText(name);
-                        }
-                    })
-                );
-            }
-        );
-    }
+export default {
+  name: "CopyDisplayName",
+  description: "Copy user's display name",
+  start() {
+    console.log("CopyDisplayName plugin started");
+  },
+  stop() {
+    console.log("CopyDisplayName plugin stopped");
+  }
 };
